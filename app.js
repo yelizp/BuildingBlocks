1310580b5ac115f62164529dd67f085bcb5b9783
Yeliz Pehlivanoglu <yeliz.isik@gmail.com>
2017-10-20 13:23:48 +0300

    Creates index.html under static public folder and adds some tests for the index.html. Adds bootstrap library

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function(req, res) {
-    res.sendStatus(200).end();
+app.use(express.static(__dirname + '/public'));
+
+app.get('/cities', function(request, response) {
+    var cities = ['Lotopia', 'Caspina', 'Indigo'];
+    response.json(cities);
 });
 
 module.exports = app;
+
--- test.js
+++ test.js
@@ -11,6 +11,37 @@ describe('Requests to the root path', function() {
                 done();
         });
     });
+
+    it('Returns a HTML format', function(done) {
+        request(app)
+            .get('/')
+            .expect('Content-Type', /html/, done);
+    });
+
+    it('Returns an index file with cities', function(done) {
+        request(app)
+            .get('/')
+            .expect(/cities/i, done);
+    });
 });
 
+describe('Listing cities on /cities', function() {
 
+    it('Returns 200 status code', function(done) {
+        request(app)
+            .get("/cities")
+            .expect(200,done);
+    });
+
+    it('Returns JSON format', function(done) {
+        request(app)
+            .get('/cities')
+            .expect('Content-Type', /json/, done);
+    });
+
+    it('Returns initial cities', function(done) {
+        request(app)
+            .get('/cities')
+            .expect(JSON.stringify(['Lotopia', 'Caspina', 'Indigo']), done);
+    });
+});
