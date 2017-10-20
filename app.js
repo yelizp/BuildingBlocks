7f6cf4c3fbe21c314875f6e0fd549737be608570
Yeliz Pehlivanoglu <yeliz.isik@gmail.com>
2017-10-20 15:05:49 +0300

    Adds custom styles

--- app.js
+++ app.js
@@ -2,6 +2,12 @@ var express = require('express');
 var app = express();
 
 app.use(express.static(__dirname + '/public'));
+app.use('/scripts', express.static(__dirname + '/node_modules/bootstrap/dist/js'));
+
+app.use('/styles', express.static(__dirname + '/styles'));
+app.use('/styles', express.static(__dirname + '/node_modules/bootstrap/dist/css'));
+
+app.use('/fonts', express.static(__dirname + '/node_modules/bootstrap/dist/fonts'));
 
 app.get('/cities', function(request, response) {
     var cities = ['Lotopia', 'Caspina', 'Indigo'];
