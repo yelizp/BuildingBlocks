4d6d64c49b3223e31f050af2436e3e48d4d5a3f7
Yeliz Pehlivanoglu <yeliz.isik@gmail.com>
2017-10-20 15:19:28 +0300

    Removes popper library and updates static paths for scripts

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
 var express = require('express');
 var app = express();
 
+//Static HTML files
 app.use(express.static(__dirname + '/public'));
-app.use('/scripts', express.static(__dirname + '/node_modules/bootstrap/dist/js'));
 
+//Scripts
+app.use('/scripts', express.static(__dirname + '/node_modules/scripts'));
+app.use('/scripts', express.static(__dirname + '/node_modules/bootstrap/dist/js'));
+app.use('/scripts', express.static(__dirname + '/node_modules/jquery/dist'));
+
+//Styles
 app.use('/styles', express.static(__dirname + '/styles'));
 app.use('/styles', express.static(__dirname + '/node_modules/bootstrap/dist/css'));
 
+//Fonts
 app.use('/fonts', express.static(__dirname + '/node_modules/bootstrap/dist/fonts'));
 
 app.get('/cities', function(request, response) {
